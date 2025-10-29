a0eefd16a5b23beb25b8e9c0bb49eddc58523d7a
ilaaaaaaaa <162697755+ilaaaaaaaa@users.noreply.github.com>
2025-10-29 09:17:16 +0100

    Metodo per l'Insert

--- src/Database.java
+++ src/Database.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
     // connetti
@@ -11,5 +9,69 @@ public class Database {
         String url = "jdbc:sqlite:database/sushi.db";
         connection = DriverManager.getConnection(url);
         System.out.println("Connessione al database");
+    }
+
+    public String selectAll() {
+        String result = "";
+
+        // isValid controlla che la connessione sia ancora valida
+        try {
+            if (connection == null || !connection.isValid(5)){
+                System.err.println("Errore di connessione al database");
+                return null;
+            }
+        } catch (SQLException e) {
+            System.err.println("Errore di connessione al database");
+            return null;
+        }
+
+        String query = "SELECT * FROM menu";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet piatti = statement.executeQuery(); // Dato che ritorna un insieme di dati
+            // lo cicliamo
+            while (piatti.next()) {
+                result += piatti.getString("id") + "\t";
+                result += piatti.getString("piatto") + "\t";
+                result += piatti.getString("prezzo") + "\t";
+                result += piatti.getString("quantita") + "\n";
+            }
+        } catch (SQLException e) {
+            System.err.println("Errore di query: " + e.getMessage());
+            return null;
+        }
+        return result;
+    }
+
+    public boolean insert(String nomePiatto, float prezzo, int quantita) {
+        // isValid controlla che la connessione sia ancora valida
+        try {
+            if (connection == null || !connection.isValid(5)){
+                System.err.println("Errore di connessione al database");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println("Errore di connessione al database");
+            return false;
+        }
+
+        String query = "INSERT INTO menu(piatto, prezzo, quantita) VALUES (?, ?, ?)";
+        System.out.println(query);
+
+        // L'SQL injection si effettua difficilmente perché se si inseriscono altre istruzioni vengono incluse nel nome del piatto
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+
+            stmt.setString(1, nomePiatto);
+            stmt.setFloat(2, prezzo);
+            stmt.setInt(3, quantita);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Errore di query: " + e.getMessage());
+            return false;
+        }
+
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
 import java.sql.SQLException;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Database db;
+        Database db = null;
         try {
             db = new Database();
         } catch (SQLException ex) {
             System.err.println("Errore di connessione al database: " + ex.getMessage());
             System.exit(-1);
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci il nome del piatto da inserire: ");
+        String nomePiatto = sc.nextLine();
+
+        System.out.println("Inserisci il prezzo del piatto da inserire: ");
+        float prezzo = sc.nextFloat();
+
+        System.out.println("Inserisci la quantità del piatto da inserire: ");
+        int quantita = sc.nextInt();
+        sc.nextLine();
+
+        if(db.insert(nomePiatto, prezzo, quantita)) {
+            System.out.println("Piatto inserito con successo");
+        }
+
+        // if(db.insert("Udon frutti di mare", 5, 1))
+       //     System.out.println("Piatto inserito con successo");
+
+        System.out.println(db.selectAll());
     }
 }
